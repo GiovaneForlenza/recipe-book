c476a387c8f94c93fc0925bb84759a36974a6e2e
GiovaneForlenza <giovane.franciscoforl@gmail.com>
2021-09-29 17:04:08 -0400

    Initial setup and CSS

---
+++ src/components/Recepies.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import recepies from '../recepies'
+
+
+function Recepies() {
+    return (
+        <div className='recepies'>
+            {recepies.map((recepie) => {
+                return <h1>aaa</h1>
+            })}
+        </div>
+    )
+}
+
+export default Recepies
